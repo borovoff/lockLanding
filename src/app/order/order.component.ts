ff45bb18ac80dd20d9ef1db97dbeee0d6f2d8c0c
borovoff <borovov.alexandr@gmail.com>
2019-01-24 22:53:19 +0300

    add form and call to server

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,10 @@
 import {Component} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+
+interface TrueResponse {
+    result: boolean;
+}
 
 @Component({
     selector: 'app-order',
@@ -7,4 +13,27 @@ import {Component} from '@angular/core';
 })
 export class OrderComponent {
 
+    courses$: Observable<TrueResponse[]>;
+    name = '';
+    mail = '';
+    phone = '';
+
+    constructor(private http: HttpClient) { }
+
+
+    send() {
+        const url = 'https://sharing.tzar.su/rest/user/addmail?name=' + this.name + '&mail=' +
+            this.mail + '&phone=' + this.phone;
+
+        console.log(url);
+
+        this.http.get(url).subscribe((res) => {
+            console.log(res);
+        });
+
+        // this.courses$ = this.http
+        //     .get<TrueResponse[]>(url)
+        //     .map(data => _.values(data))
+        //     .do(console.log);
+    }
 }
