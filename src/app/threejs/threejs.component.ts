70379b2ec8d738706b95f49fc7d3e95ed63e9bf4
andrshab <andrshab0@gmail.com>
2019-01-28 22:05:15 +0300

    comment some code in 3d model

--- src/app/threejs/threejs.component.ts
+++ src/app/threejs/threejs.component.ts
@@ -200,22 +200,22 @@ export class ThreejsComponent implements AfterViewInit, OnDestroy {
         this.controls.center.y = 0.08;
         this.controls.update();
 
-        this.makeTextSprite = this.makeTextSprite.bind(this);
-        const spritey1 = this.makeTextSprite( ' ENTER - разобрать ',
-        { fontsize: 35, fontface: 'Helvetica', borderColor: {r: 255, g: 255, b: 255, a: 1.0} } );
-        spritey1.position.set(0.25, 0.22, 0.1);
+        // this.makeTextSprite = this.makeTextSprite.bind(this);
+        // const spritey1 = this.makeTextSprite( ' ENTER - разобрать ',
+        // { fontsize: 35, fontface: 'Helvetica', borderColor: {r: 255, g: 255, b: 255, a: 1.0} } );
+        // spritey1.position.set(0.25, 0.22, 0.1);
         // this.scene.add( spritey1 );
-        const spritey2 = this.makeTextSprite( ' ESC - собрать ',
-        { fontsize: 35, fontface: 'Helvetica', borderColor: {r: 255, g: 255, b: 255, a: 1.0} } );
-        spritey2.position.set(0.25, 0.2, 0.1);
+        // const spritey2 = this.makeTextSprite( ' ESC - собрать ',
+        // { fontsize: 35, fontface: 'Helvetica', borderColor: {r: 255, g: 255, b: 255, a: 1.0} } );
+        // spritey2.position.set(0.25, 0.2, 0.1);
         // this.scene.add( spritey2 );
 
-        const textureLoader = new THREE.TextureLoader();
-        const mapB = textureLoader.load( 'assets/textures/info.png' );
-        const materialB = new THREE.SpriteMaterial( { map: mapB, color: 0xffffff, fog: true } );
-        const sprite1 = new THREE.Sprite( materialB );
-        sprite1.position.set( 0.28, 0.27, 0.15 ); // 0.3, 0.375, 0.55
-        sprite1.scale.set(0.07, 0.07, 0.07);
+        // const textureLoader = new THREE.TextureLoader();
+        // const mapB = textureLoader.load( 'assets/textures/info.png' );
+        // const materialB = new THREE.SpriteMaterial( { map: mapB, color: 0xffffff, fog: true } );
+        // const sprite1 = new THREE.Sprite( materialB );
+        // sprite1.position.set( 0.28, 0.27, 0.15 ); // 0.3, 0.375, 0.55
+        // sprite1.scale.set(0.07, 0.07, 0.07);
         // this.scene.add( sprite1 );
 
 
@@ -229,76 +229,76 @@ export class ThreejsComponent implements AfterViewInit, OnDestroy {
     }
 
 
-    makeTextSprite( message, parameters ) {
-        if ( parameters === undefined ) {
-            parameters = {};
-        }
+    // makeTextSprite( message, parameters ) {
+    //     if ( parameters === undefined ) {
+    //         parameters = {};
+    //     }
 
-        const fontface = parameters.hasOwnProperty('fontface') ?
-            parameters['fontface'] : 'Arial';
+    //     const fontface = parameters.hasOwnProperty('fontface') ?
+    //         parameters['fontface'] : 'Arial';
 
-        const fontsize = parameters.hasOwnProperty('fontsize') ?
-            parameters['fontsize'] : 18;
+    //     const fontsize = parameters.hasOwnProperty('fontsize') ?
+    //         parameters['fontsize'] : 18;
 
-        const borderThickness = parameters.hasOwnProperty('borderThickness') ?
-            parameters['borderThickness'] : 4;
+    //     const borderThickness = parameters.hasOwnProperty('borderThickness') ?
+    //         parameters['borderThickness'] : 4;
 
-        const borderColor = parameters.hasOwnProperty('borderColor') ?
-            parameters['borderColor'] : { r: 0, g: 0, b: 0, a: 1.0 };
+    //     const borderColor = parameters.hasOwnProperty('borderColor') ?
+    //         parameters['borderColor'] : { r: 0, g: 0, b: 0, a: 1.0 };
 
-        const backgroundColor = parameters.hasOwnProperty('backgroundColor') ?
-            parameters['backgroundColor'] : { r: 255, g: 255, b: 255, a: 0.0 };
+    //     const backgroundColor = parameters.hasOwnProperty('backgroundColor') ?
+    //         parameters['backgroundColor'] : { r: 255, g: 255, b: 255, a: 0.0 };
 
-        const canvas = document.createElement('canvas');
-        canvas.height = 256;
-        canvas.width = 512;
-        const context = canvas.getContext('2d');
-        context.font =  fontsize + 'px ' + fontface;
+    //     const canvas = document.createElement('canvas');
+    //     canvas.height = 256;
+    //     canvas.width = 512;
+    //     const context = canvas.getContext('2d');
+    //     context.font =  fontsize + 'px ' + fontface;
 
-        // get size data (height depends only on font size)
-        const metrics = context.measureText( message );
-        const textWidth = metrics.width;
+    //     // get size data (height depends only on font size)
+    //     const metrics = context.measureText( message );
+    //     const textWidth = metrics.width;
 
-        // background color
-        context.fillStyle   = 'rgba(' + backgroundColor.r + ',' + backgroundColor.g + ','
-                                    + backgroundColor.b + ',' + backgroundColor.a + ')';
-        // border color
-        context.strokeStyle = 'rgba(' + borderColor.r + ',' + borderColor.g + ','
-                                    + borderColor.b + ',' + borderColor.a + ')';
-        context.lineWidth = borderThickness;
-        this.roundRect(context, borderThickness / 2, borderThickness / 2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
-        // 1.4 is extra height factor for text below baseline: g,j,p,q.
+    //     // background color
+    //     context.fillStyle   = 'rgba(' + backgroundColor.r + ',' + backgroundColor.g + ','
+    //                                 + backgroundColor.b + ',' + backgroundColor.a + ')';
+    //     // border color
+    //     context.strokeStyle = 'rgba(' + borderColor.r + ',' + borderColor.g + ','
+    //                                 + borderColor.b + ',' + borderColor.a + ')';
+    //     context.lineWidth = borderThickness;
+    //     this.roundRect(context, borderThickness / 2, borderThickness / 2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+    //     // 1.4 is extra height factor for text below baseline: g,j,p,q.
 
-        // text color
-        context.fillStyle = 'rgba(255, 255, 255, 1.0)';
-        context.fillText( message, borderThickness, fontsize + borderThickness);
+    //     // text color
+    //     context.fillStyle = 'rgba(255, 255, 255, 1.0)';
+    //     context.fillText( message, borderThickness, fontsize + borderThickness);
 
-        // canvas contents will be used for a texture
-        const texture = new THREE.Texture(canvas);
-        texture.needsUpdate = true;
-        const spriteMaterial = new THREE.SpriteMaterial(
-            { map: texture, color: 0xffffff, fog: false } );
-        const sprite = new THREE.Sprite( spriteMaterial );
-        sprite.scale.set(0.1, 0.07, 0.1);
-        return sprite;
-    }
+    //     // canvas contents will be used for a texture
+    //     const texture = new THREE.Texture(canvas);
+    //     texture.needsUpdate = true;
+    //     const spriteMaterial = new THREE.SpriteMaterial(
+    //         { map: texture, color: 0xffffff, fog: false } );
+    //     const sprite = new THREE.Sprite( spriteMaterial );
+    //     sprite.scale.set(0.1, 0.07, 0.1);
+    //     return sprite;
+    // }
 
-    // function for drawing rounded rectangles
-    roundRect(ctx, x, y, w, h, r) {
-        ctx.beginPath();
-        ctx.moveTo(x + r, y);
-        ctx.lineTo(x + w - r, y);
-        ctx.quadraticCurveTo(x + w, y, x + w, y + r);
-        ctx.lineTo(x + w, y + h - r);
-        ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
-        ctx.lineTo(x+r, y+h);
-        ctx.quadraticCurveTo(x, y+h, x, y+h-r);
-        ctx.lineTo(x, y+r);
-        ctx.quadraticCurveTo(x, y, x+r, y);
-        ctx.closePath();
-        ctx.fill();
-        ctx.stroke();
-    }
+    // // function for drawing rounded rectangles
+    // roundRect(ctx, x, y, w, h, r) {
+    //     ctx.beginPath();
+    //     ctx.moveTo(x + r, y);
+    //     ctx.lineTo(x + w - r, y);
+    //     ctx.quadraticCurveTo(x + w, y, x + w, y + r);
+    //     ctx.lineTo(x + w, y + h - r);
+    //     ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+    //     ctx.lineTo(x+r, y+h);
+    //     ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+    //     ctx.lineTo(x, y+r);
+    //     ctx.quadraticCurveTo(x, y, x+r, y);
+    //     ctx.closePath();
+    //     ctx.fill();
+    //     ctx.stroke();
+    // }
 
     loadingCompleted(gltf) {
         const folder = this.folders[this.i];
