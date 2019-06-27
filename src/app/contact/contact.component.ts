d89a227443022cc70b29f7fe5b5b39fe9a0e298f
borovoff <borovov.alexandr@gmail.com>
2019-06-28 00:08:14 +0300

    contact for apple

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,20 +8,25 @@ import {DomSanitizer} from '@angular/platform-browser';
     styleUrls: ['../app.component.scss']
 })
 export class ContactComponent {
-    contacts = [
+    socials = [
         {name: 'lock.tzar', href: 'https://instagram.com/lock.tzar', svgIcon: 'instagram'},
         {name: 'lock.tzar', href: 'https://vk.com/lock.tzar', svgIcon: 'vk'},
-        {name: 'tzarsharing@gmail.com', href: 'mailto:tzarsharing@gmail.com', svgIcon: 'mail'},
         {name: 'locktzar', href: 'https://t.me/locktzar', svgIcon: 'telegram'},
+    ];
+
+    contacts = [
+        {name: '+7(952)273-44-96', href: 'tel:79522734496', svgIcon: 'phone'},
+        {name: 'andrshab', href: 'https://t.me/andrshab', svgIcon: 'telegram'},
+        {name: 'tzarsharing@gmail.com', href: 'mailto:tzarsharing@gmail.com', svgIcon: 'mail'},
     ];
 
     constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
         const path = 'assets/images/';
 
-        for (const contact of this.contacts) {
+        for (const social of this.socials) {
             this.matIconRegistry.addSvgIcon(
-                contact.svgIcon,
-                this.domSanitizer.bypassSecurityTrustResourceUrl(path + contact.svgIcon + '.svg')
+                social.svgIcon,
+                this.domSanitizer.bypassSecurityTrustResourceUrl(path + social.svgIcon + '.svg')
             );
         }
     }
